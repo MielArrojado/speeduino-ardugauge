fa8a1887ddac8da7519d405513d9c56ca4d2d4b0
Miel Arrojado <smarrojado@gmail.com>
2021-09-27 18:42:32 +0800

    removed some pages

--- software/ardugauge/ardugauge.ino
+++ software/ardugauge/ardugauge.ino
@@ -2,7 +2,7 @@
 #include "Arduino.h"
 #include "Pages.h"
 #include "Comms.h"
-#define N_PAGES 14
+#define N_PAGES 10
 
 uint8_t pageNum = EEPROM.read(0);
 
@@ -47,8 +47,9 @@ void loop()
   lastRefresh = millis();
 
   // convert temperature;
-  // int16_t value = (((int32_t)getByte(7) - 32) * 500) / 900;
-  int16_t value = (int16_t)(getByte(7) * 5.0 / 9.0) - 32;
+  // int16_t clt = (((int32_t)getByte(7) - 32) * 500) / 900;
+  int16_t clt = ((int16_t)(getByte(7) - 32) * 5.0 / 9.0);
+  // int16_t clt = getByte(7);
 
   // display pages
   switch (pageNum)
@@ -60,56 +61,41 @@ void loop()
     showBar(F("MAP (kPa)"), getWord(4), 0, 101);
     break;
   case 2:
-    showBar(F("AFR"), getByte(10), 80, 210, 1);
+    showBar(F("Coolant Temp (\367C)"), clt, 0, 120);
     break;
   case 3:
-    showBar(F("Coolant Temp (\367C)"), value, 0, 120);
-    break;
-  case 4:
-    showBar(F("Oil Pressure (psi)"), getByte(104), 0, 100);
-    break;
-  case 5:
     showBar(F("Battery (V)"), getByte(9), 60, 160, 1);
     break;
-  case 6:
+  case 4:
     showNumeric(F("Speed (kph)"), getWord(100), 0, 300);
     break;
-  case 7:
+  case 5:
     show2Bar(F("Oil Pressure (psi)"), getByte(104), 0, 100, 0,
              F("Engine Speed (RPM)"), getWord(14), 0, 6000, 0);
     break;
-  case 8:
-    show2Bar(F("Dwell(ms)"), getByte(3), 0, 200, 1,
-             F("Battery (V)"), getByte(9), 60, 160, 1);
-    break;
-  case 9:
-    showBar(F("Advance (deg)"), (int8_t)getByte(23), -10, 40);
-    break;
-  case 10:
+  case 6:
     show4Numeric(F("RPM"), getWord(14), 0, 6000, 0,
                  F("MAP (kpa)"), getWord(4), 0, 120, 0,
                  F("AFR"), getByte(10), 80, 210, 1,
                  F("Adv (deg)"), (int8_t)getByte(23), -10, 40, 0);
-    //  F(""), 0, 0, 1, 0);
     break;
-  case 11:
-    show4Numeric(F("CLT (\367C)"), value, 0, 120, 0,
+  case 7:
+    show4Numeric(F("CLT (\367C)"), clt, 0, 120, 0,
                  F("EOP (psi)"), getByte(104), 0, 100, 0,
                  F("Batt (V)"), getByte(9), 60, 160, 1,
-                 //  F(""), 0, 0, 1, 0);
-                 F("Dwell (ms)"), (int8_t)getByte(23), -10, 40, 0);
+                 F("Dwell (ms)"), (int8_t)getByte(3), 0, 200, 1);
     break;
-  case 12:
+  case 8:
     showFlags(F("crank"), getBit(2, 1),
               F("RUN"), getBit(2, 0),
               F("sync"), getBit(31, 7),
               F("warm"), getBit(2, 3),
-              F(""), getBit(31, 3),
-              F(""), getBit(31, 2),
               F(""), false,
-              F("OIL"), getBit(83, 2));
+              F(""), false,
+              F(""), false,
+              F(""), false);
     break;
-  case 13:
+  case 9:
     show4Numeric(F("Cycles/sec"), getWord(25), 0, 1000, 0,
                  F("Mem (b)"), getWord(27), 0, 2048, 0,
                  F("FPS"), refreshRate, 0, 100, 0,
