74e2aafdb88738339485ba8afe61da06619c2579
MielArrojado <smarrojado@gmail.com>
2022-09-14 08:41:36 +0800

    button hold to go to previous page

--- software/ardugauge/ardugauge.ino
+++ software/ardugauge/ardugauge.ino
@@ -2,7 +2,7 @@
 #include "Arduino.h"
 #include "Pages.h"
 #include "Comms.h"
-#define N_PAGES 10
+#define BUTTON_HOLD_TIME 500
 
 uint8_t pageNum = EEPROM.read(0);
 
@@ -21,15 +21,39 @@ void loop()
 {
   // button and page number operations
   static bool buttonLast = false;
+  static bool buttonHeld = false;
+  static uint32_t timePressed = 0;
+
   bool buttonNow = !digitalRead(2);
   digitalWrite(LED_BUILTIN, buttonNow);
+
+  // button pressed
+  if (!buttonLast & buttonNow)
+  {
+    timePressed = millis();
+  }
+  // button held
+  if (buttonNow & !buttonHeld & ((millis() - timePressed) > BUTTON_HOLD_TIME))
+  {
+    pageNum = EEPROM.read(0);
+    pageNum--;
+    if (pageNum >= N_PAGES)
+      pageNum = N_PAGES - 1;
+    EEPROM.update(0, pageNum);
+    buttonHeld = true; // prevent rerunning button held action.
+  }
+  // button released
   if (buttonLast & !buttonNow)
   {
+    if (!buttonHeld) // do not perform if button hold action executed.
+    {
     pageNum = EEPROM.read(0);
     pageNum++;
     if (pageNum >= N_PAGES)
       pageNum = 0;
     EEPROM.update(0, pageNum);
+    }
+    buttonHeld = false;
   }
   buttonLast = buttonNow;
 
